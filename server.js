0bf3943102c5dd5809610e480a47e9a48317a55e
js802025 <30957566+js802025@users.noreply.github.com>
2022-08-01 18:31:56 -0500

    made new

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const nodeGeocoder = require("node-geocoder")
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
+const admin = require("firebase-admin");
 
 const app = express()
 const port = process.env.PORT || 80
@@ -14,69 +14,78 @@ let options = {
   provider: 'openstreetmap'
 };
 
+
 let geocoder = nodeGeocoder(options);
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+var serviceAccount = require("./turtlemap-6340f-firebase-adminsdk-4hipk-f8d268ac42.json")
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://turtlemap-6340f-default-rtdb.firebaseio.com"
 });
+
+var db = admin.database()
+// client.on('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}!`);
+// });
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/web'));
 
-
+app.use(express.static(__dirname + '/dist'));
 app.get("/", (req, res) => {
-//  console.log("hello")
-  res.render("index.ejs")
+    res.render("new.ejs")
 })
 const server = app.listen(port)
-var io = require("socket.io")(server)
-io.on("connection", (socket) => {
-  updateLocations(socket)
-})
+// var io = require("socket.io")(server)
+// io.on("connection", (socket) => {
+//   updateLocations(socket)
+// })
 
-async function fetchAddress(params) {
-  return new Promise(async (resolve, reject) => {
-    var response = await fetch("http://api.positionstack.com/v1/forward?access_key=da447e4587b30998f4525e786849edbe&query="+encodeURIComponent(params))
-    var body = await response.text()
-  //  console.log(body)
-    resolve(JSON.parse(body).data)
-  })
-}
+// async function fetchAddress(params) {
+//   return new Promise(async (resolve, reject) => {
+//     var response = await fetch("http://api.positionstack.com/v1/forward?access_key=da447e4587b30998f4525e786849edbe&query="+encodeURIComponent(params))
+//     var body = await response.text()
+//     console.log(params)
+//     console.log(body)
+//     resolve(JSON.parse(body).data)
+//   })
+// }
 
-function updateLocations(socket) {
+// function updateLocations(socket) {
 
-  var channel = client.channels.cache.get("963292322123112448")
-  var locs = new Promise((resolve, reject) => {
-    var locations = {}
-    channel.messages.fetch({limit:100}).then(messages => {
-    var o = 0;
-    messages.forEach((message, i) => {
-    //  console.log(Array.from(message.reactions.cache.keys()))
-      if (!Array.from(message.reactions.cache.keys()).includes("🤔")) {
-      fetchAddress(message.content).then((res) => {
-        if (res.length > 0) {
-          var [attach] = message.attachments.values()
-          var url = attach.url
-          locations[message.content] = {longitude:res[0].longitude, latitude:res[0].latitude, tooltip:{content:"<p>"+message.content+"</p><img width='75%' src='"+url+"'/>"}, href:"https://discord.com/channels/803804129646215200/963292322123112448/"+message.id}
-        }
-      //  console.log(o)
-        if (o === messages.size -1) resolve(locations)
-        o++
-      }).catch((err) => {
-        console.log(err)
-      })} else {
-        if (o === messages.size -1) resolve(locations)
-        o++
-      }
-    });
-  })
-})
-locs.then((locations) => {
-  //console.log(locations)
-  socket.emit("locs", locations)
-})
-}
+//   var channel = client.channels.cache.get("963292322123112448")
+//   var locs = new Promise((resolve, reject) => {
+//     var locations = {}
+//     channel.messages.fetch({limit:100}).then(messages => {
+//     var o = 0;
+//     messages.forEach((message, i) => {
+//     //  console.log(Array.from(message.reactions.cache.keys()))
+//       if (!Array.from(message.reactions.cache.keys()).includes("🤔")) {
+//       fetchAddress(message.content).then((res) => {
+//         if (res.length > 0) {
+//           var [attach] = message.attachments.values()
+//           var url = attach.url
+//           locations[message.content] = {longitude:res[0].longitude, latitude:res[0].latitude, imgsrc:url, href:"https://discord.com/channels/803804129646215200/963292322123112448/"+message.id}
+//         }
+//       //  console.log(o)
+//         if (o === messages.size -1) resolve(locations)
+//         o++
+//       }).catch((err) => {
+//         console.log(err)
+//       })} else {
+//         if (o === messages.size -1) resolve(locations)
+//         o++
+//       }
+//     });
+//   })
+// })
+// locs.then((locations) => {
+//   //console.log(locations)
+//   socket.emit("locs", locations)
+// })
+// }
 
 
 
-client.login("ODgwODQzNTAyOTQ5MzgwMTc2.YSkLcA.K60o7kAnSv8EOQ21aUjWim0NXrw")
+// client.login("ODgwODQzNTAyOTQ5MzgwMTc2.YSkLcA.K60o7kAnSv8EOQ21aUjWim0NXrw")
---
+++ web/index.js
@@ -0,0 +1,114 @@
+import { initializeApp } from "firebase/app";
+import { GeocoderAutocomplete } from '@geoapify/geocoder-autocomplete';
+import { getDatabase, set, push, get,ref as database_ref } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChYOck4VcEdS81trsBoortqyzmtUFoHYo",
+  authDomain: "turtlemap-6340f.firebaseapp.com",
+  projectId: "turtlemap-6340f",
+  storageBucket: "turtlemap-6340f.appspot.com",
+  messagingSenderId: "806157536569",
+  appId: "1:806157536569:web:04a167f6a731064e4e2ce8"
+};
+const app = initializeApp(firebaseConfig);
+
+
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  getStorage
+} from "firebase/storage";
+const storage = getStorage();
+const database = getDatabase(app);
+// Initialize Firebase
+
+//const socket = io()
+
+var map = L.map('map').setView([0, 0], 2.4);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+
+var fileUpload = document.getElementById("fileButton")
+
+fileUpload.addEventListener("change", (e) => {
+  var file = e.target.files[0]
+
+  const storageRef = ref(storage, `/files/${file.name}`)
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+          const percent = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          );
+
+          // update progress
+          setPercent(percent);
+      },
+      (err) => console.log(err),
+      () => {
+          // download url
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+              $("#imgurl").val(url)
+          });
+      }
+  ); 
+})
+
+
+const autocomplete = new GeocoderAutocomplete(
+  document.getElementById("autocomplete"), 
+  '59061a072f6241a485feda9ef9f1319c', 
+  { /* Geocoder options */ });
+
+autocomplete.on('select', (location) => {
+ // console.log(location)
+  $("#lat").val(location.properties.lat)
+  $("#lon").val(location.properties.lon)
+  $("#address").val(location.properties.formatted)
+  $("#submit").prop('disabled', false);
+});
+
+autocomplete.on('suggestions', (suggestions) => {
+// process suggestions here
+});
+
+function setPercent(percent) {
+  $("#percent").css("width", percent + "%")
+}
+
+
+
+$().ready(() => {
+ // document.getElementsByClassName(".geoapify-autocomplete-input")[0].classList.add("form-control")
+ $("#submit").on("click", (e) => {
+  const turtleRef = database_ref(database, "turtles/")
+  const turtleId = push(turtleRef)
+  set(turtleId, {
+    title: $("#address").val(),
+    lat: $("#lat").val(),
+    lon: $("#lon").val(),
+    imgurl: $("#imgurl").val(),
+  });
+ })
+
+ get(database_ref(database, "turtles/")).then((snapshot) => {
+  var turtles = snapshot.val()
+  Object.entries(turtles).forEach(([key, value]) => {
+    L.marker([value.lat, value.lon], {
+      color: "red"
+    }).addTo(map)
+    .bindPopup('<img src="'+value.imgurl+'" width="75%"><br>'+value.title)
+    .openPopup();
+  })
+})
+
+})
